7ae36819d1351cd42c69cf02ca3c30bcb0e1b0ee
Derek <heyderek@dereknelson.net>
2012-10-17 12:34:46 -0700

    Changes to portfolio section.

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   $('#accordion h4').click(function(){
     var showArticle = $(this).next('article'), 
     hideArticle = $('#accordion article');
-    
     if (! $(this).hasClass('open')) {
       $('#accordion h4').removeClass('open');
       $(this).addClass('open');
@@ -13,9 +12,13 @@ $(document).ready(function() {
       $('#accordion h4').removeClass('open');
    }
   });
-//Scrollto functionality for navigation
+  //Scrollto functionality for navigation
  $('a#home').bind('click', function(){
    $.scrollTo('#about',{'duration':'slow'});
+   return false;
+ })
+ $('a.button').bind('click', function(){
+   $.scrollTo('#contact',{'duration':'slow'});
    return false;
  })
  $('a#work').bind('click', function(){
@@ -30,12 +33,14 @@ $(document).ready(function() {
    $.scrollTo('#contact',{'duration':'slow'});
    return false;
  })
- //Activate Carousel
+  //Activate Carousel
  $('#carousel_wrap').carousel({ dispItems: 4 });
+ 
  $('#carousel img').click(function(){
     var showArticle = $(this).next('article'), 
     hideArticle = $('#carousel article');
     if (! $(this).hasClass('active')) {
+      $('#carousel img').removeClass('active');
       $(this).addClass('active');
       hideArticle.hide();
       showArticle.slideDown('slow');

9b346ed91f84e81da6e7109b8ee8ba1227c3734f
Derek <heyderek@dereknelson.net>
2012-10-17 08:46:57 -0700

    changes to the accordion.

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
   //Accordion functionality
-  $('#accordion h4').click(function(){
-    var showArticle = $(this).next('article'), 
-    hideArticle = $('#accordion article');
-    if (! $(this).hasClass('open')) {
-      $(this).addClass('open');
-      hideArticle.hide();
-      showArticle.slideDown('slow');
-   } else {
-      $(this).removeClass('open');
-      showArticle.slideUp();
-   }
-  });
+  $('#accordion h4').click(function(){
+    var showArticle = $(this).next('article'), 
+    hideArticle = $('#accordion article');
+    if (! $(this).hasClass('open')) {
+      $(this).addClass('open');
+      hideArticle.hide();
+      showArticle.slideDown('slow');
+   } else {
+      $(this).removeClass('open');
+      showArticle.slideUp();
+   }
+  });
  //Scrollto functionality for navigation
  $('a#home').bind('click', function(){
    $.scrollTo('#about',{'duration':'slow'});
